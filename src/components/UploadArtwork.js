d9c710eb833a5d129d81cbe1a03f886274d1f5e4
Tharun1912 <145247263+Tharun1912@users.noreply.github.com>
2024-12-10 22:14:21 +0530

    Update UploadArtwork.js

--- src/components/UploadArtwork.js
+++ src/components/UploadArtwork.js
@@ -38,7 +38,7 @@ const UploadArtwork = () => {
     formData.append('image', image);
 
     try {
-      const response = await axios.post('http://localhost:8081/api/artworks/upload', formData, {
+      const response = await axios.post('artsbackend.railway.internal/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
